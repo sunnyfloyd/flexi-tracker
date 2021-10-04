369e1a52ade18cdce378883df0a85f404ab3be86
sunnyfloyd <pain.floyd.maq@gmail.com>
2021-10-04 11:32:49 +0200

    settings.py changes

--- flexitracker/flexitracker/settings.py
+++ flexitracker/flexitracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "kdfsgj-d#fgdfjgi^KIDJ*9ek-lh15zr-mw%$1%+sum59f!en3ufa9_wq4tbe1ummj7cn"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
